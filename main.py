9c99b3033043ccd9f954e466ddf0e4f668d4eae5
Meri Liis Treimann <meri@MacBook-Pro-2.local>
2020-12-13 20:00:43 +0200

    Possible to specify part when printing answers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 from datetime import datetime
 
 from solutions.utils.print_answers import print_answers
@@ -7,8 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        print_answers(day=int(sys.argv[1]), year=2020)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('day', type=int)
+    parser.add_argument('-p', '--part', default=None, type=str)
+    args = parser.parse_args()
+
+    if args.day:
+        print_answers(day=args.day, year=2020, part=args.part)
     else:
         current_time = datetime.now()
         for time in [datetime(2020, 12, i, 7) for i in range(1, 32)]:
--- solutions/utils/print_answers.py
+++ solutions/utils/print_answers.py
@@ -1,17 +1,12 @@
+from typing import Optional
 import importlib
 from aocd import get_data
 
 from .timing import timeit
 
 
-def print_answers(day: int, year: int) -> None:
-    data = get_data(day=day, year=year)
-
-    print(f'-- 📅 {year} day {day} --')
-
-    module = importlib.import_module(f'solutions.day_{day}.solution')
-
-    for part in ['a', 'b']:
+def print_answers(day: int, year: int, part: Optional[str]) -> None:
+    def print_part_answers(data: str, module: str, part: str) -> None:
         part_fun = getattr(module, f'get_part_{part}_answer', None)
         if part_fun:
             answer, time = timeit(part_fun)(data)
@@ -19,3 +14,16 @@ def print_answers(day: int, year: int) -> None:
                   f'⏱️  {time:.3f} ms\n')
         else:
             print(f'Part {part} is not solved yet... ⏳️')
+
+    data = get_data(day=day, year=year)
+
+    print(f'-- 📅 {year} day {day} --')
+
+    module = importlib.import_module(f'solutions.day_{day}.solution')
+
+    if part is not None:
+        print_part_answers(data, module, part)
+
+    else:
+        print_part_answers(data, module, 'a')
+        print_part_answers(data, module, 'b')
